489d09f972bfbc15407a93b556e291db053b1bd4
SRINATH <SRINATH@SRINATH>
2022-10-22 09:29:07 +0530

    third

--- Testing/src/AbstractHelper.java
+++ Testing/src/AbstractHelper.java
@@ -1,12 +1,30 @@
+package Bank;
+import java.util.Scanner;
+public class BankDriver 
+{
 
-
-abstract public class AbstractHelper {
-	
-	abstract public void creatAcc();
-	abstract public void login();
-	abstract public void deposit();
-	abstract public void withdraw();
-	abstract public void showBal();
-	abstract public void printDetails();
-
+	static Scanner s=new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		Helper h=new Helper();
+		
+		boolean logout=true;
+		while(logout)
+		{
+			System.out.println("Enter \n1.create account\n2.login\n3.Logout");
+			int cho=s.nextInt();
+			switch(cho)
+			{
+			case 1:h.creatAcc();
+			break;
+			case 2:h.login();
+			break;
+			case 3:logout=false;
+			break;
+			default : System.out.println("Enter a  valid option ");
+			
+			}
+		}
+		
+	}
 }
